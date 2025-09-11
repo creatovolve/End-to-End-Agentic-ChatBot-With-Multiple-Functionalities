781851d008598075299c93428b363c535b4e43c7
creatovolve <dnp234@gmail.com>
2025-09-11 14:27:49 +0530

    Commit 11Sep-2

--- src/ChatBotWebSearch/ui/streamlitui/loadui.py
+++ src/ChatBotWebSearch/ui/streamlitui/loadui.py
@@ -24,8 +24,17 @@ class LoadUI:
                 self.user_choices["GROQ_API_KEY"]=st.session_state["GROQ_API_KEY"]=st.text_input("API Key", type='password')
             self.user_choices["selected_usecase"]=st.selectbox("Select Usecases", use_case_options)
 
-            if self.user_choices["selected_usecase"] == "Chatbot with Web":
+            if self.user_choices["selected_usecase"] == "Chatbot with Web" or self.user_choices["selected_usecase"] == "AI News Summary":
                 os.environ["TAVILY_API_KEY"] = self.user_choices["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"] = st.text_input("TAVILY API KEY", type="password")
 
+            if self.user_choices["selected_usecase"] == "AI News Summary":
+                st.subheader("AI News Summary")
+
+                with st.sidebar:
+                    time_frame = st.selectbox("Select Time Frame", ["Daily", "Weekly", "Monthly"], index=0)
+
+                if st.button("Fetch News"):
+                    st.session_state.time_frame = time_frame
+                    st.session_state.IsFetchButtonPressed = True 
 
         return self.user_choices
